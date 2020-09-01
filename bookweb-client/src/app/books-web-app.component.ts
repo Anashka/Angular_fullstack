0d9796397f73b532716dc2d2a4f4626c558a7360
Anashka <anashkanaina@gmail.com>
2020-09-01 12:07:26 -0600

    book-we-client- assignment

--- bookweb-client/src/app/books-web-app.component.ts
+++ bookweb-client/src/app/books-web-app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-booksweb',  //custom html element or component identifier
     styleUrls:[
-        'books-web-app.component.css'
+        'books-web-app.component.css'///no styles as of now
     ],
     templateUrl:'books-web-app.component.html'
 })
