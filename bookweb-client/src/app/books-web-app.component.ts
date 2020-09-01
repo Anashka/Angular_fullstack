193cf19e923d74255fa9eb3b106bce07b024058d
Anashka <anashkanaina@gmail.com>
2020-09-01 12:05:38 -0600

    book-we-client#day2-01

---
+++ bookweb-client/src/app/books-web-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-booksweb',  //custom html element or component identifier
+    styleUrls:[
+        'books-web-app.component.css'
+    ],
+    templateUrl:'books-web-app.component.html'
+})
+export class BooksWebAppComponent{
+    
+}
